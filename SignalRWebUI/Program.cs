6c06ae78b82c72c125680a18f289e2f68bf21ee3
koraytopuz <krytopuz@gmail.com>
2025-02-17 17:52:44 +0300

    .

--- SignalRWebUI/Program.cs
+++ SignalRWebUI/Program.cs
@@ -6,8 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddHttpClient();
 builder.Services.AddControllersWithViews();
-builder.Services.AddDbContext<SignalRContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 var app = builder.Build();
 
